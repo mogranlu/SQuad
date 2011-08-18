426344982097feb8da9db451ce53f91049e1fe10
Morten Granlund <morten.granlund@gmail.com>
2011-08-18 17:42:15 +0200

    Adding some groovy stuff

---
+++ src/groovy/maven_project/src/main/groovy/mg/jz/groovy/Example.groovy
@@ -0,0 +1,15 @@
+//
+// Generated from archetype; please customize.
+//
+
+package mg.jz.groovy
+
+/**
+ * Example Groovy class.
+ */
+class Example
+{
+    def show() {
+        println 'Hello World'
+    }
+}
