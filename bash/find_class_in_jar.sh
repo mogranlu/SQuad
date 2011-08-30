0b1f5c3e53fb9c876550c883e8590dbe74efaba5
Morten Granlund <morten.granlund@gmail.com>
2011-08-30 13:59:07 +0200

    some change in jar-finder, and renamed mkstock to mkchecksums

--- bash/find_class_in_jar.sh
+++ bash/find_class_in_jar.sh
@@ -1,8 +1,8 @@
+#Attempt #1 : (fails)
 find /D/maven_repository/ -name "*.jar" -exec unzip -l '{}' \; | awk '{ if ($0 ~ /^Archive/ ) {lastjar=$NF} else if ($NF ~ /ControlException.class/ ) { exit } }; END {print lastjar}'
 
-#Not quite:
-find /C/Users/Morten/.m2/repository -name "*.jar" -exec unzip -l '{}' \; | awk 'BEGIN {result=""};{ if ($0 ~ /^Archive/ ) {print "Searching archive", $NF; lastjar=$NF} if ($0 ~ /\/ControlException\.class/ ) { print $NF; result=result lastjar } }; END {print result}'
-# Something fishy happens with the resulting string!
+#Attempt #2 : (Weird behaviour in git bash, but works in linux bash shells like gnome-terminal)
+find /usr/share/openoffice/basis3.2/help/ -name "*.jar" -exec unzip -l '{}' \; | awk 'BEGIN {result="Found in: "};{ if ($0 ~ /^Archive/ ) {print "Searching archive", $NF; lastjar=$NF} if ($0 ~ /03090800.xhp/ ) { print $NF; result=result "\n" lastjar } }; END {print result}'
 
-# Lots of output:
+# Attempt #3 : Lots of output, but works OK in git bash on Windows machines:
 searchClass="DefaultPluginManager"; searchDir="/C/Users/Morten/.m2/repository" ; find ${searchDir} -name "*.jar" -exec echo "...Searching: {}" \; -exec jar -tf '{}' \; | egrep -e "^\.\.\.Searching|${searchClass}"
